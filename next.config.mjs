40df8264b35159ea1a3fb05cedff9a5393ff72c5
Dileepa Mabulage <dileepainivos@gmail.com>
2024-05-26 22:06:27 +0530

    add workflow and change the next configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: 'build',
+  output: 'standalone',
+};
 
 export default nextConfig;
